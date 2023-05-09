5cf11158d5584db9ad93b09839cd76ae03f58ca6
Mayurwagh98 <mswagh1998@gmail.com>
2023-05-09 14:00:26 +0530

    Changed: images src

--- assignment/src/pages/Profile.jsx
+++ assignment/src/pages/Profile.jsx
@@ -12,8 +12,14 @@ const Profile = () => {
       </div>
       <div className="details_div">
         <div>
-          <img src="src/Images/Ellipse 114.png" alt="" />
-          <img src="src/Images/Group 1585.svg" alt="" />
+          <img
+            src="https://user-images.githubusercontent.com/69896733/237038737-7b9771bf-d1ce-4d36-835e-e91776db6a7f.png"
+            alt="image"
+          />
+          <img
+            src="https://user-images.githubusercontent.com/69896733/237038782-0737284c-5d44-4cbf-921b-d7906a402526.svg"
+            alt="svg"
+          />
         </div>
         <div>
           <h3>Marry Doe</h3>
